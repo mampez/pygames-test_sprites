8f923e3f57895ea8b0cbc6b32502cf21a6a92ab5
menchopez <mmp23uc@gmail.com>
2017-07-18 12:35:57 +0200

    change some commentaries and variable names

--- test.py
+++ test.py
@@ -20,17 +20,16 @@ K = 0.5
  # Clases
 # ---------------------------------------------------------------------
  
-class Bola(pygame.sprite.Sprite):
-
+class ball(pygame.sprite.Sprite):
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
+        pygame.sprite.Sprite.__init__(self) ## Inheritance: Call the parent class (Sprite) constructor
         self.image = load_image("images/ball.png", True)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.centery = HEIGHT / 2
-        self.speed = [SPEED_X, SPEED_Y]        ##x,y speed
+        self.speed = [SPEED_X, SPEED_Y]        ## x,y speed
  
-    def actualizar(self, time, pala_jug, pala_cpu, puntos):
+    def update(self, time, racket_jug, racket_cpu, scores):
 
         """ We calculate the ball position based on  e = v*t  """
         self.rect.centerx += self.speed[0] * time
@@ -38,17 +37,19 @@ class Bola(pygame.sprite.Sprite):
 
         ## Check if the ball touch any side --> Puntuation and Restart ball
         if self.rect.left <= 0:
-            puntos[1] += 1
+            scores[1] += 1
             self.rect.centerx = WIDTH / 2
             self.rect.centery = HEIGHT / 2
             self.speed = [SPEED_X + K * random.random(), SPEED_Y + K * random.random()]
             print self.speed
         if self.rect.right >= WIDTH:
-            puntos[0] += 1
+            scores[0] += 1
             self.rect.centerx = WIDTH / 2
             self.rect.centery = HEIGHT / 2
             self.speed = [SPEED_X + K * random.random(), SPEED_Y + K * random.random()]
             print self.speed
+
+        ## Collisions!
 
         ## Collision PARED
         if self.rect.left <= 0 or self.rect.right >= WIDTH:
@@ -57,21 +58,20 @@ class Bola(pygame.sprite.Sprite):
         if self.rect.top <= 0 or self.rect.bottom >= HEIGHT:
             self.speed[1] = -self.speed[1]
             self.rect.centery += self.speed[1] * time
-
         ## Collision JUGADORES
-        if pygame.sprite.collide_rect(self, pala_jug):
+        if pygame.sprite.collide_rect(self, racket_jug):
             self.speed[0] = -self.speed[0]
             self.rect.centerx += self.speed[0] * time
-        if pygame.sprite.collide_rect(self, pala_cpu):
+        if pygame.sprite.collide_rect(self, racket_cpu):
             self.speed[0] = -self.speed[0]
             self.rect.centerx += self.speed[0] * time
 
-        return puntos
+        return scores
  
-class Pala(pygame.sprite.Sprite):
+class racket(pygame.sprite.Sprite):
     def __init__(self, x):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = load_image("images/pala.png")
+        pygame.sprite.Sprite.__init__(self)		## Inheritance: Call the parent class (Sprite) constructor
+        self.image = load_image("images/racket.png")
         self.rect = self.image.get_rect()
         self.rect.centerx = x
         self.rect.centery = HEIGHT / 2
@@ -97,14 +97,14 @@ class Pala(pygame.sprite.Sprite):
 # ---------------------------------------------------------------------
  
 def load_image(filename, transparent=False):
-        try: image = pygame.image.load(filename)
-        except pygame.error, message:
-                raise SystemExit, message
-        image = image.convert()
-        if transparent:
-                color = image.get_at((0,0))
-                image.set_colorkey(color, RLEACCEL)
-        return image
+    try: image = pygame.image.load(filename)
+    except pygame.error, message:
+            raise SystemExit, message
+    image = image.convert()
+    if transparent:
+            color = image.get_at((0,0))
+            image.set_colorkey(color, RLEACCEL)
+    return image
 
 def crear_texto(texto, posx, posy, color=(255, 255, 255)):
     fuente = pygame.font.Font("C:\Windows\Fonts\Arial.ttf", 25)
@@ -121,15 +121,15 @@ def main():
     pygame.display.set_caption("Pruebas Pygame")
 
     ##Puntuation
-    puntos = [0,0]
+    scores = [0,0]
     
     ## Create background
     background_image = load_image('images/fondo_pong.png')
 
     ## Call objets: ball, player and ball
-    bola = Bola()
-    pala_jug = Pala(30)
-    pala_cpu = Pala(WIDTH - 30)
+    ball = ball()
+    racket_jug = racket(30)
+    racket_cpu = racket(WIDTH - 30)
  
     clock = pygame.time.Clock()
  
@@ -141,24 +141,25 @@ def main():
                 sys.exit(0)
  
         ## Update ball position, player and enemy. It returns the score of both players!
-        puntos = bola.actualizar(time, pala_jug, pala_cpu, puntos)
+        scores = ball.update(time, racket_jug, racket_cpu, scores)
 
         ## Update player position --> key
-        pala_jug.mover(time, keys)
+        racket_jug.mover(time, keys)
 
         ## Update cpu position --> ia
-        pala_cpu.ia(time, bola)
+        racket_cpu.ia(time, ball)
 
-        p_jug, p_jug_rect = crear_texto(str(puntos[0]), WIDTH/4, 40)
-        p_cpu, p_cpu_rect = crear_texto(str(puntos[1]), WIDTH-WIDTH/4, 40)
+        ## Print score
+        p_jug, p_jug_rect = crear_texto(str(scores[0]), WIDTH/4, 40)
+        p_cpu, p_cpu_rect = crear_texto(str(scores[1]), WIDTH-WIDTH/4, 40)
 
         ## Re-draw elements
-        screen.blit(background_image, (0, 0))       ## Background
-        screen.blit(p_jug, p_jug_rect)              ## Player puntuation
-        screen.blit(p_cpu, p_cpu_rect)              ## CPU puntuations
-        screen.blit(bola.image, bola.rect)          ## Ball
-        screen.blit(pala_jug.image, pala_jug.rect)  ## Pala
-        screen.blit(pala_cpu.image, pala_cpu.rect)  ## CPU
+        screen.blit(background_image, (0, 0))       	## Background
+        screen.blit(p_jug, p_jug_rect)              	## Player puntuation
+        screen.blit(p_cpu, p_cpu_rect)              	## CPU puntuations
+        screen.blit(ball.image, ball.rect)          	## Ball
+        screen.blit(racket_jug.image, racket_jug.rect)  ## racket
+        screen.blit(racket_cpu.image, racket_cpu.rect)  ## CPU
 
         ## Update all changes
         pygame.display.flip()
